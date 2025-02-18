7fb5663b1e2be166f92859954ef35a97c8efc9ee
TayLee <thlee991@gmail.com>
2025-02-18 14:14:51 +0900

    commit) front vue

---
+++ vue-lecture/project/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+
+    // 위의 경우 import를 하게 된다. 따라서 해당 Tab을 눌러 Router기능을 만든다면, app.js -> about2.js를 불러오게 된다. (그때 불러오는 이름을 webpackChunkName으로 조절 할 수 있다)
+    // component: () => import(/* webpackChunkName: "about2" */ '../views/AboutView.vue')
+
+    // 아래 webpackPreFetch Option이란 무엇인가?
+    component: () => import(/* webpackChunkName: "about2", webpackPrefetch:true */ '../views/AboutView.vue')
+
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
