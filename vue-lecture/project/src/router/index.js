727da60a9743b1faa0a6f41e6c149ddc099db50d
TayLee <thlee991@gmail.com>
2025-02-19 18:15:55 +0900

    commit)

--- vue-lecture/project/src/router/index.js
+++ vue-lecture/project/src/router/index.js
@@ -36,6 +36,11 @@ const routes = [
     path: '/databinding/input',
     name: 'DataBindingInputView',
     component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingInputView.vue')
+  },
+  {
+    path: '/databinding/select',
+    name: 'DataBindingSelectView',
+    component: () => import(/* webpackChunkName: "databinding", webpackPrefetch:true */ '../views/1_databinding/DataBindingSelectView.vue')
   }
 ]
 
--- vue-lecture/project/src/views/1_databinding/DataBindingInputView.vue
+++ vue-lecture/project/src/views/1_databinding/DataBindingInputView.vue
@@ -3,10 +3,15 @@
     <input type="text" v-model="userId" />
     <button @click="myFunction();">클릭</button>
     <button @click="changeData();">변경</button>
-    <br>
+    <br />
     <input type="text" v-model="num1" /> +
     <input type="text" v-model="num2" /> =
-    <span></span>
+    <span>{{ num1 + num2 }}</span>
+    <br />
+    <!-- 숫자 양방향 데이터 binding은 .number를 써서 사용한다 -->
+    <input type="text" v-model.number="num3" /> +
+    <input type="text" v-model.number="num4" /> =
+    <span>{{ num3 + num4 }}</span>
   </div>
 </template>
 <script>
