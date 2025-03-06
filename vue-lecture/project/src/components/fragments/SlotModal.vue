2409543219a1057310306986af7ebb6a18ca08f1
TayLee <thlee991@gmail.com>
2025-03-06 10:41:13 +0900

    commit) slot modal 사용

--- vue-lecture/project/src/components/fragments/SlotModal.vue
+++ vue-lecture/project/src/components/fragments/SlotModal.vue
@@ -1,28 +1,36 @@
 <template>
     <div>
-        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
+      <!-- id의 경우 staticBackDrop이라고 박혀 있을 경우 여러 모달창을 띄워야 할때 띄울수가 없게 된다. 이것 또한 props로 받게 하자 aria-label 이 부분도 temporal literal method ${} back tick과 함께 사용 -->
+      <div class="modal fade" :id="modalId" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" :aria-labelledby="`${modalId}Label`" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" :id="`${modalId}Label`">
+                      <slot name="title"></slot>
+                    </h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <slot name="body"></slot>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Understood</button>
+                    <slot name="footer"></slot>
                 </div>
-                </div>
-            </div>
-            </div>
+              </div>
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   components: {},
+  props: {
+    modalId: {
+      type: String,
+      default: 'myModal'
+    }
+  },
   data() {
     return {
       sampleData: ''
--- vue-lecture/project/src/views/4_reuse/SlotView.vue
+++ vue-lecture/project/src/views/4_reuse/SlotView.vue
@@ -1,7 +1,53 @@
 <template>
     <div>
-        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Show Modal</button>
-        <SlotModal></SlotModal>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#userModal">Show Modal</button>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#userModal2">Show Modal2</button>
+        <SlotModal modalId="userModal">
+          <template v-slot:title>모달창 타이틀</template> <!-- 자식 component내의 slot의 name속성이 title인것을 찾아서 안의 내용을 넣어준다 -->
+
+          <template v-slot:body> <!-- 자식 component내의 slot의 name속성이 body를 찾아서 안의 내용을 넣어준다 -->
+            <div class="row gy-2"> <!-- BootStrap: 여기서 그냥 "row" 아래로 나열 -> class="row gy-2" 이미 css에서 아래로 자간을 두는듯 -->
+                <div class="col-3">
+                  <label for="" class="form-label">이메일 주소</label>
+                </div>
+                <div class="col-9">
+                  <input type="email" name="" id="" class="form-control">
+                </div>
+                <div class="col-3">
+                  <label for="" class="form-label">전화번호</label>
+                </div>
+                <div class="col-9">
+                  <input type="tel" name="" id="" class="form-control">
+                </div>
+                <div class="col-3">
+                  <label for="" class="form-label">주소</label>
+                </div>
+                <div class="col-9">
+                  <input type="text" name="" id="" class="form-control">
+                </div>
+            </div>
+          </template>
+
+          <template v-slot:footer>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+            <button type="button" class="btn btn-primary">저장</button>
+          </template>
+        </SlotModal>
+
+        <SlotModal modalId="userModal2">
+          <template v-slot:title>모달창 타이틀</template> <!-- 자식 component내의 slot의 name속성이 title인것을 찾아서 안의 내용을 넣어준다 -->
+
+          <template v-slot:body> <!-- 자식 component내의 slot의 name속성이 body를 찾아서 안의 내용을 넣어준다 -->
+            <p>문장1</p>
+            <p>문장2</p>
+            <p>문장3</p>
+          </template>
+
+          <template v-slot:footer>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+            <button type="button" class="btn btn-primary">저장</button>
+          </template>
+        </SlotModal>
     </div>
 </template>
 
